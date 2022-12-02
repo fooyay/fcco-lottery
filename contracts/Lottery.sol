804f83a19b0ab4e6e0d75acf41114ff7f1620ef0
Sean Coates <sean.coates+fooyay@gmail.com>
2022-12-02 18:01:44 -0500

    add deploy scripts

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -15,7 +15,18 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 error Lottery__NotEnoughETHEntered();
 error Lottery__TransferFailed();
 error Lottery__NotOpen();
+error Lottery__UpkeepNotNeeded(
+    uint256 currentBalance,
+    uint256 numPlayers,
+    uint256 lotteryState
+);
 
+/**
+ * @title A sample Lottery contract
+ * @author Sean Coates
+ * @notice This contract enables people to participate in a lottery, without automatic payouts
+ * @dev This implments Chainlink VRF v2 and Chainklink Keepers
+ */
 contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
     /* Type declarations */
     enum LotteryState {
@@ -44,8 +55,9 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
     event RequestedLotteryWinner(uint256 indexed requestId);
     event WinnerPicked(address indexed winner);
 
+    /* Functions */
     constructor(
-        address vrfCoordinatorV2,
+        address vrfCoordinatorV2, // contract
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
@@ -84,9 +96,9 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
      * 4. The lottery should be in an "open" state
      */
     function checkUpkeep(
-        bytes calldata /* checkData */
+        bytes memory /* checkData */
     )
-        external
+        public
         override
         returns (bool upkeepNeeded, bytes memory /* performData */)
     {
@@ -97,10 +109,18 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
         upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
     }
 
-    function requestRandomWinner() external {
+    function performUpkeep(bytes calldata /* performData */) external override {
         // request the random number
         // once we get it, do something with it
         // 2 transaction process
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Lottery__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_lotteryState)
+            );
+        }
         s_lotteryState = LotteryState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane, // gasLane
@@ -121,6 +141,7 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
         s_recentWinner = recentWinner;
         s_lotteryState = LotteryState.OPEN;
         s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         // require(success)
         if (!success) {
@@ -140,5 +161,25 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
 
     function getRecentWinner() public view returns (address) {
         return s_recentWinner;
+    }
+
+    function getLotteryState() public view returns (LotteryState) {
+        return s_lotteryState;
+    }
+
+    function getNumWords() public pure returns (uint256) {
+        return NUM_WORDS;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getLatestTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRequestConfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,46 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "0x00"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmatoins: 1,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [GOERLI_PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmatoins: 2,
+            gas: 6000000,
+        },
+    },
     solidity: "0.8.17",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
 }
